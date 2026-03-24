f4558d117789e50caeb10962829e82b0fd140852
Deyvid <deyvid.guedes.fer@gmail.com>
2026-03-24 00:28:39 -0300

    refactor: remove o PatternFormat para corrigir conflitos com o hook-form

--- src/components/ui/FormInputField/FormInputField.tsx
+++ src/components/ui/FormInputField/FormInputField.tsx
@@ -1,31 +1,57 @@
 import styles from "./formField.module.css";
 import { InputHTMLAttributes } from "react";
-import { PatternFormat, PatternFormatProps } from "react-number-format";
 
-interface ComponentProps extends Omit<PatternFormatProps, "format"> {
+export const masks = {
+  cep: (value: string) => {
+    return value
+      .replace(/\D/g, "")
+      .replace(/(\d{5})(\d)/, "$1-$2")
+      .replace(/(-\d{3})\d+?$/, "$1");
+  },
+  tel: (value: string) => {
+    return value
+      .replace(/\D/g, "")
+      .replace(/(\d{2})(\d)/, "($1) $2")
+      .replace(/(\d{5})(\d)/, "$1-$2")
+      .replace(/(-\d{4})\d+?$/, "$1");
+  },
+};
+
+interface ComponentProps extends InputHTMLAttributes<HTMLInputElement> {
   errorMessage?: string;
-  mask?: string;
+  maskType?: "cep" | "tel";
+  label?: string;
 }
 
 export default function FormInputField({
   errorMessage,
-  mask,
+  maskType,
+  label,
+  onChange,
   ...rest
-}: ComponentProps) 
+}: ComponentProps) {
+  
+  // Função que intercepta a digitação e aplica a máscara antes de salvar no form
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (maskType && masks[maskType]) {
+      event.target.value = masks[maskType](event.target.value);
+    }
+    
+    // Dispara o onChange original do react-hook-form
+    if (onChange) {
+      onChange(event);
+    }
+  };
 
-{
   return (
     <div className={styles.formInput}>
-      {mask ? (
-        <PatternFormat
-          {...rest}
-          format={mask}
-          mask="_"
-          customInput={(props) => <input {...props} />} // ComponentType<InputAttributes> exige algo redenrizável
-        />
-      ) : (
-        <input {...rest as InputHTMLAttributes<HTMLInputElement>} />
-      )}
+      {label && <label>{label}</label>}
+      
+      <input 
+        {...rest} 
+        onChange={handleChange} 
+        className={errorMessage ? styles.inputError : ""}
+      />
       
       {errorMessage && (
         <p className={styles.errorLabel}>{errorMessage}</p>
